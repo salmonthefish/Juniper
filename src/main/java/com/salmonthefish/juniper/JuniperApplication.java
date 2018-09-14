4c8b2364bbac6e62850213b0204b7cf3c394af5e
Greg Salmon <salmonthefish@gmail.com>
2018-09-13 23:06:45 -0400

    initial commit

---
+++ src/main/java/com/salmonthefish/juniper/JuniperApplication.java
@@ -0,0 +1,12 @@
+package com.salmonthefish.juniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniperApplication.class, args);
+	}
+}
